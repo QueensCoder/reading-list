4d7c9a0bfee234b7abfc8af27ec2bd77f93bf3b1
Ozal Khan <ozalkhan196@gmail.com>
2019-12-14 13:10:35 -0500

    implemented local storage and use effect to make books persistant

--- booklist/src/contexts/BookContext.js
+++ booklist/src/contexts/BookContext.js
@@ -1,10 +1,21 @@
-import React, { createContext, useReducer } from "react";
+import React, { createContext, useReducer, useEffect } from "react";
 import bookReducer, { addBook, removeBook } from "../reducer/booksReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = props => {
-  const [books, dispatch] = useReducer(bookReducer, []);
+  // use reducer takes a third argument, thiis function checks to see if there is
+  // data inside of local storage if so the state becomes what is in local storage
+  const [books, dispatch] = useReducer(bookReducer, [], () => {
+    const localData = localStorage.getItem("books");
+    return localData ? JSON.parse(localData) : [];
+  });
+
+  // whenever the dom is updated the local storage is changed to reflect what is inside
+  // the store, so when a book is removed from the dom it is also removed from local storage
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
 
   return (
     <BookContext.Provider value={{ books, dispatch, addBook, removeBook }}>
