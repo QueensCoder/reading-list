7043d63720fc0ef15492b3671a5717599f3aa546
Ozal Khan <ozalkhan196@gmail.com>
2019-12-17 22:39:55 -0500

    needed to import @testing-library/jest-dom/extend-expect, sort of clunky lib to use

--- booklist/src/tests/BookContext.test.js
+++ booklist/src/tests/BookContext.test.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
+
+import "@testing-library/jest-dom/extend-expect";
+// need to import this or else toHaveTextContent wont work
+
 import { BookContext } from "../contexts/BookContext";
 import BookList from "../components/BookList";
 
-test("allows user to add a an entry by providing the books and the author", async () => {
-  const testTitle = "test";
-  const testAuthor = "author";
-  const books = [{ title: "abcc", author: "123", id: 1 }];
-  const { getByText } = render(
+test("if no books are provided the book list displays message of : 'No books to read. Hello free time'", () => {
+  const books = [];
+  render(
     <BookContext.Provider value={{ books }}>
       <BookList />
     </BookContext.Provider>
   );
+  expect(
+    screen.getByText(/^No books to read. Hello free time/)
+  ).toHaveTextContent("No books to read. Hello free time");
 });
