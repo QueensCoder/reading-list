49d3fa8dba3f9f844bdbd843707ef8b272b9a040
Ozal Khan <ozalkhan196@gmail.com>
2019-12-18 23:42:58 -0500

    need to test the new book list and form together to make sure entries are being added after manipulating the dom

--- src/tests/NewBookForm.test.js
+++ src/tests/NewBookForm.test.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useReducer } from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 
 import "@testing-library/jest-dom/extend-expect";
@@ -6,22 +6,28 @@ import "@testing-library/jest-dom/extend-expect";
 
 import { BookContext } from "../contexts/BookContext";
 import NewBookForm from "../components/NewBookForm";
+import bookReducer, { addBook } from "../reducer/booksReducer";
 
-//allows form to add
-test("if delete button is clicked on an entry, the entry is removed from the book list", () => {
-  const books = [
-    // { title: "Art of War", author: "Sun Tzu", id: 2 }
-  ];
-  render(
-    <BookContext.Provider value={{ books }}>
+const TestNewBookForm = () => {
+  const [books, dispatch] = useReducer(bookReducer, []);
+  return (
+    <BookContext.Provider value={{ books, addBook, dispatch }}>
       <NewBookForm />
     </BookContext.Provider>
   );
+};
+
+//allows form to add
+test("if delete button is clicked on an entry, the entry is removed from the book list", () => {
+  render(<TestNewBookForm />);
   fireEvent.change(screen.getByLabelText(/^title/), {
     target: { value: "Art of War" }
   });
-  //   fireEvent.change(screen.getByLabelText(/^author/), {
-  //     target: { value: "Art of War" }
-  //   });
-  //   fireEvent.click(screen.getByText(/^add book/));
+  fireEvent.change(screen.getByLabelText(/^author/), {
+    target: { value: "Sun Tzu" }
+  });
+  fireEvent.click(screen.getByText(/^add book/));
+
+  expect(screen.getByText(/^Art of War/)).toHaveTextContent("Art of War");
+  expect(screen.getByText(/^Sun Tzu/)).toHaveTextContent("Sun Tzu");
 });
